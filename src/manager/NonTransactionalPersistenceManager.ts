13404e08e6be76e219364885ec19e63f9a8f7373
Jasper Blues <jasper@liberation-data.com>
2019-10-20 11:00:17 +0800

    Add stub.

--- src/manager/NonTransactionalPersistenceManager.ts
+++ src/manager/NonTransactionalPersistenceManager.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from "@nestjs/common";
 import { DrivineError } from '@/DrivineError';
 import { PersistenceManager } from '@/manager/PersistenceManager';
 import { QuerySpecification } from '@/query/QuerySpecification';
 import { ConnectionProvider } from '@/connection/ConnectionProvider';
 import { InjectConnectionProvider } from '@/DrivineInjectionDecorators';
+import { Cursor } from "@/cursor/Cursor";
 
 @Injectable()
 export class NonTransactionalPersistenceManager implements PersistenceManager {
+
+    private logger = new Logger(NonTransactionalPersistenceManager.name);
+
     public constructor(@InjectConnectionProvider() public readonly connectionProvider: ConnectionProvider) {}
 
     public async query<T>(spec: QuerySpecification<T>): Promise<T[]> {
@@ -18,5 +22,12 @@ export class NonTransactionalPersistenceManager implements PersistenceManager {
         } finally {
             await connection.release();
         }
+    }
+
+    public async openCursor<T>(spec: QuerySpecification<T>): Promise<Cursor<T>> {
+        this.logger.verbose(`Open consumer for ${spec}`);
+        return new Promise((resolve, reject) => {
+            reject(new DrivineError(`Not implemented yet, please use TransactionalPersistenceManager`));
+        });
     }
 }
--- src/manager/PersistenceManager.ts
+++ src/manager/PersistenceManager.ts
@@ -1,5 +1,10 @@
 import { QuerySpecification } from '@/query/QuerySpecification';
+import { Cursor } from "@/cursor/Cursor";
 
 export interface PersistenceManager {
+
     query<T>(spec: QuerySpecification<T>): Promise<T[]>;
+
+    openCursor<T>(spec: QuerySpecification<T>): Promise<Cursor<T>>;
+
 }
