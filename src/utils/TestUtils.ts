8c37d5829d2260f7ce5d52b6d5aa377951a8e578
Jasper Blues <jasper@liberation-data.com>
2020-03-04 13:51:50 +0800

    Fix bug in RunWithDrivine jest hooks.

--- src/utils/TestUtils.ts
+++ src/utils/TestUtils.ts
@@ -1,4 +1,4 @@
-import { inDrivineContext } from '@/context/DrivineContext';
+import { DrivineContext, inDrivineContext } from '@/context/DrivineContext';
 import { TransactionOptions } from '@/transaction/Transactional';
 
 export interface DrivineRunnerOptions {
@@ -7,20 +7,34 @@ export interface DrivineRunnerOptions {
 
 export const RunWithDrivine = (options?: DrivineRunnerOptions): void => {
     if (!global['$$runWithDrivine$$']) {
-        const testMethodsToOverride = ['it', 'test'];
-        const lifecycleMethodsToOverride = ['beforeAll', 'beforeEach', 'afterAll', 'afterEach'];
-
         const drivineContext = inDrivineContext();
         if (options && options.transaction) {
             drivineContext.withTransaction(options.transaction);
         }
 
-        [...testMethodsToOverride, ...lifecycleMethodsToOverride].forEach(methodName => {
-            const original = global[methodName];
-            global[methodName] = (name: string, fn?: any, timeout?: number) => {
-                original(name, async () => drivineContext.run(fn), timeout);
-            };
-        });
+        hookLifecycleMethods(drivineContext);
+        hookTestMethods(drivineContext);
+
         global['$$runWithDrivine$$'] = true;
     }
 };
+
+function hookLifecycleMethods(drivineContext: DrivineContext): void {
+    const lifecycleMethodsToOverride = ['beforeAll', 'beforeEach', 'afterAll', 'afterEach'];
+    lifecycleMethodsToOverride.forEach(methodName => {
+        const original = global[methodName];
+        global[methodName] = (fn?: any, timeout?: number)  => {
+            original(async () => drivineContext.run(fn), timeout);
+        };
+    });
+}
+
+function hookTestMethods(drivineContext: DrivineContext): void {
+    const testMethodsToOverride = ['it', 'test'];
+    testMethodsToOverride.forEach(methodName => {
+        const original = global[methodName];
+        global[methodName] = (name: string, fn?: any, timeout?: number) => {
+            original(name, async () => drivineContext.run(fn), timeout);
+        };
+    });
+}
