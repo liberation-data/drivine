bbbab029d3d15d0646223bec13faa28c0a3b4c51
Jasper Blues <jasper@liberation-data.com>
2020-09-06 14:56:46 +0800

    Add primitive props options.

--- src/utils/ObjectUtils.ts
+++ src/utils/ObjectUtils.ts
@@ -1,13 +1,20 @@
+import { classToPlain } from 'class-transformer';
+
+export interface PrimitivePropsOptions {
+    classToPlain: boolean;
+}
+
 export class ObjectUtils {
     /**
      * Returns a new object that contains only the 'primitive' properties, or arrays of them, from the source object.
      * @param object
      * @returns {{}}
      */
-    static primitiveProps = (object: any): any => {
+    static primitiveProps = (object: any, options: PrimitivePropsOptions = { classToPlain: true }): any => {
         const props = {};
         if (object) {
-            Object.keys(object).forEach((key: string) => {
+            const source = options.classToPlain ? classToPlain(object) : object
+            Object.keys(source).forEach((key: string) => {
                 const candidate = object[key];
                 // Array of primitives
                 if (candidate != undefined && candidate.constructor === Array
